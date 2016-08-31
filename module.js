b5108505c32dd58dd0a61e8b3734827dea421941
Zeeshan Syed <zsyed89@gmail.com>
2016-08-31 16:41:58 -0400

    Added the Revealing Module Pattern

--- module.js
+++ module.js
@@ -29,5 +29,39 @@ var HTMLChanger = (function(){
     };
 })();
 
+console.log("---------Module Pattern---------");
 HTMLChanger.callChangeHTML(); // Outputs: 'contents'
 console.log(HTMLChanger.contents); // undefined
+
+/*
+    Revealing Module Pattern
+
+    Purpose of this is to maintain encapsulation and reveal certain variables
+    and methods return in an object literal.
+*/
+var Exposer = (function(){
+    var privateVariable = 10;
+
+    var privateMethod = function(){
+        console.log('Inside a private method');
+        privateMethod++;
+    };
+
+    var methodToExpose = function(){
+        console.log('This is a method I want to expose!');
+    };
+
+    var otherMethodIWantToExpose = function(){
+        privateMethod();
+    };
+
+    return {
+        first: methodToExpose,
+        second: otherMethodIWantToExpose
+    };
+})();
+
+console.log("---------Revealing Module Pattern---------");
+Exposer.first();        // Output: This is a method I want to expose
+Exposer.second();       // Output: Inside a private method
+Exposer.methodToExpose; // undefined

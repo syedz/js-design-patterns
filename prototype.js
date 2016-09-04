bc6e82d7b6b04389252b035615824b4edc6453ef
Zeeshan Syed <zsyed89@gmail.com>
2016-09-04 13:30:39 -0400

    Added more Prototype pattern details

--- prototype.js
+++ prototype.js
@@ -127,11 +127,18 @@ newTesla.printMake();
 var NewPrototype = function() {
     this.a = 1;
     this.b = 2;
+    this.doSomething = function(){
+        console.log("Doing something");
+    }
 };
 
 NewPrototype.prototype = {
     b: 3,
-    c: 4
+    c: 4,
+    d: 5,
+    doSomethingElse: function(){
+        console.log("Doing something else");
+    }
 };
 
 var o = new NewPrototype();
@@ -145,14 +152,192 @@ console.log("o.b: " + o.b); // 2
 console.log("o.[[Prototype]].b: " + o.__proto__.b); // 3
 // Is there a 'b' own property on o? Yes, and its value is 2.
 // The prototype also has a 'b' property, but it's not visited.
-// This is called "property shadowing"
+// This is called "property shadowing"; simliar to method overriding
 
 console.log("o.c: " + o.c); // 4
 // Is there a 'c' own property on o? No, check its prototype.
 // Is there a 'c' own property on o.[[Prototype]]? Yes, its value is 4.
 
 console.log("o.d: " + o.d); // undefined
+console.log("o.hasOwnProperty(d): " + o.hasOwnProperty('d')); // false
 // Is there a 'd' own property on o? No, check its prototype.
 // Is there a 'd' own property on o.[[Prototype]]? No, check its prototype.
 // o.[[Prototype]].[[Prototype]] is null, stop searching,
 // no property found, return undefined
+
+o.doSomething();
+o.doSomethingElse();
+
+/*
+    Inheriting Methods
+*/
+var o = {
+    a: 2,
+    m: function(b){
+        return this.a + 1;
+    }
+};
+
+console.log("---------Inheriting Methods---------");
+console.log(o.m()) // 3
+
+var p = Object.create(o);
+// p is an object that inherits from o
+
+p.a = 4; // Create an own poperty on p
+console.log(p.m()); // 5
+// When p.m() is called, 'this' refers to p.
+// So when p inherits the function m of o,
+// 'this.a' means p.a, the own property 'a' of p
+
+
+/*
+    Different ways to create objects and the resulting prototype chain
+*/
+
+var o = {a: 1};
+
+// The newly created object o has Object.prototype as its [[Prototype]]
+// o has no own property named 'hasOwnProperty'
+// hasOwnProperty is an own property of Object.prototype.
+// So o inherits hasOwnProperty from Object.prototype
+// Object.prototype has null as its prototype.
+// o ---> Object.prototype ---> null
+
+var a = ["yo", "whadup", "?"];
+
+// Arrays inherit from Array.prototype
+// (which has methods like indexOf, forEach, etc.)
+// The prototype chain looks like:
+// a ---> Array.prototype ---> Object.prototype ---> null
+
+function f(){
+  return 2;
+}
+
+// Functions inherit from Function.prototype
+// (which has methods like call, bind, etc.)
+// f ---> Function.prototype ---> Object.prototype ---> null
+
+/*
+    With a constructor
+*/
+
+var Graph = function(){
+    this.vertices = [];
+    this.edges    = [];
+};
+
+Graph.prototype = {
+    addVertex: function(){
+        this.vertices.push(v);
+    }
+};
+
+console.log("---------With a Contstructor---------");
+var g = new Graph();
+console.log(g);
+// g is an object, and has the properies 'verticies' and 'edges'
+// g.[[Prototype]] is the value of Graph.prototype when new Graph() is executed.
+
+
+/*
+    With Object.create
+
+    ECMAScript 5 introduced a new method: Object.create(). Calling this method creates a new object. The prototype of this object is the first argument of the function.
+*/
+
+var a = {a: 1};
+// a ---> Object.prototype ---> null
+console.log("---------With Object.create---------");
+console.log(a);
+
+var b = Object.create(a);
+// b ---> a ---> Object.prototype ---> null
+console.log(b);
+console.log(b.a); // 1 (inherited)
+
+var c = Object.create(b);
+console.log(c);
+// c ---> b ---> a ---> Object.prototype ---> null
+
+var d = Object.create(null);
+console.log(d);
+// d ---> null
+console.log(d.hasOwnProperty);
+// undefined, because d doesn't inherit from Object.prototype
+
+
+/*
+    With the class keyword
+
+    ECMAScript 6 introduced a new set of keywords implementing classes. Although these constructs look like those familiar to developers of class-based languages, they are not the same. JavaScript remains prototype-based. The new keywords include class, constructor, static, extends, and super.
+*/
+
+"use strict";
+
+class Polygon {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+}
+
+class Square extends Polygon {
+    constructor(sideLength) {
+        super(sideLength, sideLength);
+    }
+    get area() {
+        return this.height * this.width;
+    }
+    set sideLength(newLength) {
+        this.height = newLength;
+        this.width = newLength;
+    }
+}
+
+var square = new Square(2);
+
+
+/*
+    Final Example
+*/
+
+var A = function(a){
+    this.varA = a;
+};
+
+A.prototype = {
+    varA: null,
+    doSomething: function(){
+
+    }
+};
+
+var B = function(a,b){
+    A.call(this, a);
+    this.varB = b;
+};
+B.prototype = Object.create(A.prototype, {
+    varB: {
+        value:        null,
+        enumerable:   true,
+        configurable: true,
+        writable:     true
+    },
+    doSomething: {
+        value: function(){ // override
+            A.prototype.doSomething.apply(this, arguments); // call super
+            // ...
+        },
+        enumerable: true,
+        configurable: true,
+        writable: true
+    }
+});
+B.prototype.constructor = B;
+
+console.log("---------Final Example---------");
+var b = new B();
+b.doSomething();
+console.log(b);

c944d0ad179ff63c774c4d81edac4b6cadb87b3d
Zeeshan Syed <zsyed89@gmail.com>
2016-09-06 15:52:51 -0400

    Added more Module pattern content

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ Sometimes it's nice to practice the basics.
 
 Scotch.io - https://scotch.io/bar-talk/4-javascript-design-patterns-you-should-know
 
+Todd Motto - https://toddmotto.com/mastering-the-module-pattern/
+
 Mozilla - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Inheritance_and_the_prototype_chain
--- module.js
+++ module.js
@@ -77,3 +77,131 @@ Exposer.second();           // Output: Inside a private method
 Exposer.third()             // Output: 11
 console.log(Exposer.getPrivateVariable) // Output: the function literal
 Exposer.methodToExpose;     // undefined
+
+
+/*
+    Locally Scope Object Literal
+
+    Easier to see which methods are public and which are private when you have
+    long file.
+*/
+var Module1 = (function(){
+
+     // Locally scoped object
+     var myObject = {};
+
+     // Declared with `var`, must be "private"
+     var _privateMethod = function() {};
+
+     myObject.someMethod = function() {
+         // Public method
+     };
+
+     return myObject;
+})();
+
+/*
+    Another way of doing it.
+    NOTE: Not recommend;
+*/
+var Module2 = (function(){
+
+     var _privateMethod = function() {};
+
+     var myObject = {
+         someMethod: function() {},
+         anotherMethod: function() {}
+     };
+
+     return myObject;
+})();
+
+/*
+    Accessing Private Methods, Objects, Arrays, and everything else
+*/
+var Module3 = (function(){
+
+    var privateArray = [];
+
+    var _privateMethod = function(somethingOfInterest){
+        privateArray.push(somethingOfInterest);
+        console.log(privateArray);
+    };
+
+    var publicMethod = function(text){
+        _privateMethod(text);
+    };
+
+    return {
+        publicMethod: publicMethod
+    };
+})();
+Module3.publicMethod('Hello');
+
+/*
+    Augumenting Modules
+
+    Extending modules and including another smaller Module.
+*/
+var Module4 = (function(){
+
+    var _privateMethod = function() {
+        // private
+    };
+
+    var someMethod = function() {
+        // public
+    };
+
+    var anotherMethod = function(){
+        // public
+    };
+
+    return {
+        someMethod:    someMethod,
+        anotherMethod: anotherMethod
+    };
+})();
+
+// So far our Object Module looks like this:
+// Object {someMethod: function, anotherMethod: function}
+
+// We want to add our Module extension, so it ends up having another public method,
+// like this:
+// Object {someMethod: function, anotherMethod: function, extension: function}
+
+var Module4Two = (function(Module4){
+    // access to `Module`
+
+    Module4.extension = function() {
+        // another method
+    };
+
+    return Module4;
+
+})(Module4 || {});
+
+console.log(Module4);
+console.log(Module4Two);
+// Both will be the same
+
+/*
+    Employee Example
+*/
+
+console.log("----------Employee Example---------");
+var Employee = (function(){
+    var firstName = "John";
+    var lastName = "Smith";
+
+    return {
+        getFirstName: function(){
+            return firstName;
+        },
+        getLastName: function(){
+            return lastName;
+        }
+    };
+})();
+
+// console.log(Employee.getFirstName);
